9a48a4487f27700a4a38b0dc3ca4fb68d370b3fb
adambrockhaus <adam.m.brockhaus@gmail.com>
2021-08-02 10:30:24 -0500

    Added bootstrap along with the bootstrap navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,11 +7,40 @@ import {
 class Navbar extends React.Component {
     render(){
       return(
-        <div className="nav">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+          <a class="navbar-brand" href="#">Navbar</a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item active">
+              <Link to="/" class="nav-link">Home</Link>
+              </li>
+              <li class="nav-item">
+              <Link to="consultants" class="nav-link">Consultants</Link>
+              </li>
+              <li class="nav-item">
+                <Link to="booking" class="nav-link">Booking</Link>
+              </li>
+            </ul>
+          </div>
+        </nav>
+
+
+
+
+
+
+
+
+
+/*       <div className="nav">
           <Link to="/">Home</Link> | 
           <Link to="consultants">Consultants</Link> | 
           <Link to="booking">Booking</Link>
         </div>
+*/
       );
     }
   }
