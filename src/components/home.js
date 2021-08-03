d993bb5fdc356db94e7701df8583d86c10206e02
adambrockhaus <adam.m.brockhaus@gmail.com>
2021-08-02 20:51:12 -0500

    Jumbotron, contact form on home page.

--- src/components/home.js
+++ src/components/home.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import "../css/home.css";
 
 class Home extends React.Component {
     render()
     {
       return (
-        <div>
+  /*      <div>
           <h1>Money Talks</h1>
           <p>Find your perfect Career Consultant: We offer skill workshops and 1-on-1 strategy sessions. 
             <ul>
@@ -14,7 +15,44 @@ class Home extends React.Component {
             </ul>
           </p>
         </div>
+        */
+      <div>
+        <div className="jumbotron">
+          <h1 className="display-4">Money Talks</h1>
+          <p className="lead">Find your perfect Career Consultant: We off skill workshops and 1-on-1 strategy sessions.</p>
+          <hr className="my-4" />
+        </div>
+        <div>
+          <p>Finding your perfect consultant has never been easier.  Browse our list of top quality career consultants to find the one that suits your needs perfectly.
+            Once you pick a career consultant, booking an appointment is just a click away!  If you have any questions or concerns, fill out the form below and we will get back to you soon!
+          </p>
+        </div>
+        <form>
+          <div className="form-group">
+            <label htmlFor="name">Name:</label>
+            <input type="name" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="email">Email Address:</label>
+            <input type="email" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="phone">Phone number:</label>
+            <input type="phone" className="form-control" />
+          </div>
+          <div className="form-group">
+            <label htmlFor="contact">Questions or comments:</label>
+            <textarea className="form-control" rows="3"></textarea>
+          </div>
+        </form>
+      </div>
       );
     }
   }
   export default Home;
+
+  // todo 
+  // Add contact form at the bottom of the page
+  // Ask about logo image to use for navbar
+  // Create a jumbotron?
+  // 
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -7,21 +7,21 @@ import {
 class Navbar extends React.Component {
     render(){
       return(
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="#">Navbar</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <a className="navbar-brand" href="#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
           </button>
-          <div class="collapse navbar-collapse" id="navbarNav">
-            <ul class="navbar-nav">
-              <li class="nav-item active">
-              <Link to="/" class="nav-link">Home</Link>
+          <div className="collapse navbar-collapse" id="navbarNav">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+              <Link to="/" className="nav-link">Home</Link>
               </li>
-              <li class="nav-item">
-              <Link to="consultants" class="nav-link">Consultants</Link>
+              <li className="nav-item">
+              <Link to="consultants" className="nav-link">Consultants</Link>
               </li>
-              <li class="nav-item">
-                <Link to="booking" class="nav-link">Booking</Link>
+              <li className="nav-item">
+                <Link to="booking" className="nav-link">Booking</Link>
               </li>
             </ul>
           </div>
