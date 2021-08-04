a51c5622f705f50dc4273e373583287244243891
LizbethRangelLara <lizbeth.rangel@live.com>
2021-08-04 00:25:16 -0400

    Add CSS for general website and footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -4,9 +4,7 @@ class Footer extends React.Component{
     render(){
         return(
             <div className="footer">
-                <p>555 Money Talks Lane</p>
-                <p>90210</p>
-                <p>Beverly Hills, CA</p>
+                <p>555 Money Talks Lane, 90210, Beverly Hills, CA</p>
             </div>
         );
     }
