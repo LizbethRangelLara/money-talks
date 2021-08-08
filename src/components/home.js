4399a0ef4afccb8ecfba5d4da1815f3b322d4e1b
LizbethRangelLara <lizbeth.rangel@live.com>
2021-08-08 00:37:19 -0400

    CSS edit for navbar, footer, home, index

--- src/components/home.js
+++ src/components/home.js
@@ -12,7 +12,7 @@ class Home extends React.Component {
           <div>
             <Jumbotron />
           </div>
-          <div>
+          <div className="form-intro">
             <p>Finding your perfect consultant has never been easier.  Browse our list of top quality career consultants to find the one that suits your needs perfectly.
               Once you pick a career consultant, booking an appointment is just a click away!  If you have any questions or concerns, fill out the form below and we will get back to you soon!
             </p>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import "../css/navbar.css";
+
 import {
   Link
 } from 'react-router-dom';
@@ -7,8 +9,7 @@ import {
 class Navbar extends React.Component {
     render(){
       return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-          <a className="navbar-brand" href="#">Navbar</a>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
           <img src="../MTLogo.png" class="img-fluid" width="100"></img>          
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
